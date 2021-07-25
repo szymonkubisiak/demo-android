918e998f5cc957bc3163ccd536f818710ad0dfc8
Szymon Kubisiak <46302021+szymonkubisiak@users.noreply.github.com>
2021-07-25 18:22:56 +0200

    added methods to simplify ViewModel creation

--- ui/src/main/java/pl/kubisiak/ui/listtest/ListTestFragment.kt
+++ ui/src/main/java/pl/kubisiak/ui/listtest/ListTestFragment.kt
@@ -1,12 +1,11 @@
 package pl.kubisiak.ui.listtest
 
-import androidx.lifecycle.ViewModelProviders
 import pl.kubisiak.ui.R
 import pl.kubisiak.ui.BaseFragment
 
 class ListTestFragment : BaseFragment() {
     override fun createViewModel() =
-        ViewModelProviders.of(this).get(ListTestViewModel::class.java)
+        getVmTroughProvider(::ListTestViewModel)
 
     override fun getLayoutRes() =
         R.layout.listtest_fragment
--- ui/src/main/java/pl/kubisiak/ui/postslist/PostsListFragment.kt
+++ ui/src/main/java/pl/kubisiak/ui/postslist/PostsListFragment.kt
@@ -1,9 +1,6 @@
 package pl.kubisiak.ui.postslist
 
 import android.os.Bundle
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.ViewModelProviders
 import pl.kubisiak.ui.R
 import pl.kubisiak.dataflow.models.Blog
 import pl.kubisiak.ui.BaseFragment
@@ -11,16 +8,9 @@ import pl.kubisiak.ui.BaseFragment
 private const val BLOG_ID = "blogidparam"
 
 class PostsListFragment : BaseFragment() {
-    private class Factory(val id: Blog.ID) : ViewModelProvider.Factory {
-        override fun <T : ViewModel> create(modelClass: Class<T>): T {
-            return modelClass.cast(PostsListViewModel(id))!!
-        }
-    }
 
     override fun createViewModel() =
-        ViewModelProviders
-            .of(this, Factory(arguments?.getSerializable(BLOG_ID) as Blog.ID))
-            .get(PostsListViewModel::class.java)
+        getVmTroughProvider(arguments?.getSerializable(BLOG_ID) as Blog.ID, ::PostsListViewModel)
 
     override fun getLayoutRes() =
         R.layout.postslist_fragment
